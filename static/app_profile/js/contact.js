9e369c2f53adf2d04d3008a238da82413e280b6d
LucaSoriani1 <luca_soriani@hotmail.com>
2023-05-15 17:36:26 +0200

    fix js file

--- static/app_profile/js/contact.js
+++ static/app_profile/js/contact.js
@@ -1,99 +1,95 @@
 function handleSubmit(event) {
-    event.preventDefault();
-    if (checkForm()) {
-        clearValidation();
-      sendPostForm(event);
-    } else {
-      requiredFieldForm(event);
-    }
+  event.preventDefault();
+  if (checkForm()) {
+      clearValidation();
+    sendPostForm(event);
+  } else {
+    requiredFieldForm(event);
   }
-  
-  const form = document.querySelector('form');
-  form.addEventListener('submit', handleSubmit);
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
 
 function checkForm() {
-    var name = document.getElementById("id_name").value;
-    var email = document.getElementById("id_email").value;
-    var subject = document.getElementById("id_subject").value;
-    var message = document.getElementById("message").value;
-  
-    if (name === "" || email === "" || subject === "" || message === "") {
-      return false;
-    }
+  var name = document.getElementById("id_name").value;
+  var email = document.getElementById("id_email").value;
+  var subject = document.getElementById("id_subject").value;
+  var message = document.getElementById("message").value;
 
-    var emailRegex = /\S+@\S+\.\S+/;
-    if (!emailRegex.test(email)) {
-      return false;
-    }
-  
-    return true;
+  if (name === "" || email === "" || subject === "" || message === "") {
+    return false;
   }
 
-  function sendPostForm(event) {
-    event.preventDefault();
-    var form = document.getElementById("task-form");
-    var name = document.getElementById("id_name").value;
-    var email = document.getElementById("id_email").value;
-    var subject = document.getElementById("id_subject").value;
-    var message = document.getElementById("message").value;
+  var emailRegex = /\S+@\S+\.\S+/;
+  if (!emailRegex.test(email)) {
+    return false;
+  }
 
-    showLoader();
-    var fd = new FormData();
-    fd.append("name", name);
-    fd.append("email", email);
-    fd.append("subject", subject);
-    fd.append("message", message);
-    fd.append('csrfmiddlewaretoken', $('input[name=csrfmiddlewaretoken]').val());          
-    form.reset();
-    axios.post('/', fd)
-        .then(function(){
-            hideLoader();
-            toastTrigger("success");
-        })
-        .catch(function(error){
+  return true;
+}
+
+function sendPostForm(event) {
+  event.preventDefault();
+  var form = document.getElementById("task-form");
+  var name = document.getElementById("id_name").value;
+  var email = document.getElementById("id_email").value;
+  var subject = document.getElementById("id_subject").value;
+  var message = document.getElementById("message").value;
+
+  showLoader();
+  var fd = new FormData();
+  fd.append("name", name);
+  fd.append("email", email);
+  fd.append("subject", subject);
+  fd.append("message", message);
+  fd.append('csrfmiddlewaretoken', $('input[name=csrfmiddlewaretoken]').val());          
+  form.reset();
+  axios.post('/', fd)
+      .then(function(){
           hideLoader();
-          if (error.response.data.status=="error") {
-            toastTrigger("error");
-          } else {
-            toastTrigger("invalid");
-          }
+          toastTrigger("success");
+      })
+      .catch(function(error){
+        console.warn = () => {};
+        hideLoader();
+        toastTrigger(error.response.data.status);
         });
-    };
+  };
 
 function showLoader() {
-    document.getElementById("submit-button-contact").style.display = "none";
-    document.getElementById("loader").style.display = "block";
+  document.getElementById("submit-button-contact").style.display = "none";
+  document.getElementById("loader").style.display = "block";
 };
 
 function hideLoader() {
-    document.getElementById("submit-button-contact").style.display = "block";
-    document.getElementById("loader").style.display = "none";
+  document.getElementById("submit-button-contact").style.display = "block";
+  document.getElementById("loader").style.display = "none";
 };
 
 function requiredFieldForm() {
-    'use strict';
-    const forms = document.querySelectorAll('.needs-validation');
-  
-    Array.from(forms).forEach(form => {
-        form.classList.add('was-validated');
-      }, false);
-    } 
-  
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+
+  Array.from(forms).forEach(form => {
+      form.classList.add('was-validated');
+    }, false);
+  } 
+
 function toastTrigger(status) {
-    if (status=="success"){
-    const toastLiveExample = document.getElementById('liveToast');
-  } else if(status=="invalid"){
-    const toastLiveExample = document.getElementById('liveToastInvalid');
-  } else {
-    const toastLiveExample = document.getElementById('liveToastError');
-  }
-  const toast = new bootstrap.Toast(toastLiveExample);
-  toast.show();
+let toastLiveExample = document.getElementById('liveToast');
+if (status=="invalid"){
+  toastLiveExample = document.getElementById('liveToastWarning');
+} else if (status=="error") {
+  toastLiveExample = document.getElementById('liveToastError');
+}
+const toast = new bootstrap.Toast(toastLiveExample);
+toast.show();
 }
 
 function clearValidation() {
-    const forms = document.querySelectorAll('.needs-validation');
-    Array.from(forms).forEach(form => {
-      form.classList.remove('was-validated');
-    });
-  }
+  const forms = document.querySelectorAll('.needs-validation');
+  Array.from(forms).forEach(form => {
+    form.classList.remove('was-validated');
+  });
+}
